02aeed9f94a4c79e2a8b9b9127aabb6ae8012fc5
Imran Irshad <imran.ib@live.com>
2021-11-20 22:02:16 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: '@dooboo/eslint-config',
+  rules: {
+    'eslint-comments/no-unlimited-disable': 0,
+    'eslint-comments/no-unused-disable': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
